880dba1719ed247c8fe7d6498ce151322c31a25d
saunaik19 <saunaik19@gmail.com>
2022-11-16 04:16:09 +0530

    Initial commit

---
+++ src/main/java/com/saurabh/SpringSecurityJpaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJpaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJpaExampleApplication.class, args);
+	}
+
+}
